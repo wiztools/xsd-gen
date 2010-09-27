9c542034d03f4ed9c7ba620af13d1c652f02ab34
Subhash Chandran <subwiz@gmail.com>
2010-09-27 15:13:12 +0000

    Added commandline parameters.

--- src/main/java/org/wiztools/xsdgen/XsdGenMain.java
+++ src/main/java/org/wiztools/xsdgen/XsdGenMain.java
@@ -1,20 +1,85 @@
 package org.wiztools.xsdgen;
 
+import com.sampullara.cli.Args;
+import com.sampullara.cli.Argument;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.util.List;
 
 /**
  *
  * @author subWiz
  */
 public class XsdGenMain {
+
+    private static class ArgParse {
+        @Argument(value="prefix", alias="p", description="The namespace prefix for the xsd (default is `xsd').")
+        private String xsdPrefix;
+
+        @Argument(value="output", alias="o", description="Write xsd to outfile instead of STDOUT.")
+        private String xsdFile;
+
+        @Argument(value="force", alias="f", description="")
+        private boolean isForceWrite;
+
+        @Argument(value="c", alias="charset", description="Output charset.")
+        private String charset;
+
+        @Argument(value="help", alias="h", description="Print usage help.")
+        private boolean isHelp;
+    }
+
     public static void main(String[] arg) throws ParseException, IOException {
-        if(arg.length != 1) {
-            System.err.println("Accpets one parameter: XML file path.");
+        ArgParse cliParser = new ArgParse();
+        List<String> extras = Args.parse(cliParser, arg);
+        if(extras.size() != 1
+                || cliParser.isHelp) {
+            System.err.println("Usage:");
+            Args.usage(cliParser);
             System.exit(1);
         }
 
-        final File file = new File(arg[0]);
-        new XsdGen().parse(file).write(System.out);
+        final File xmlFile = new File(extras.get(0));
+
+        // XSD prefix
+        final XsdGen xsdGen;
+        if(cliParser.xsdPrefix != null) {
+            xsdGen = new XsdGen(cliParser.xsdPrefix);
+        }
+        else {
+            xsdGen = new XsdGen();
+        }
+
+        // Now parse!
+        xsdGen.parse(xmlFile);
+
+        // Charset
+        final Charset charset;
+        if(cliParser.charset != null) {
+            charset = Charset.forName(cliParser.charset);
+        }
+        else {
+            charset = Charset.defaultCharset();
+        }
+
+        // Output file
+        final OutputStream os;
+        if(cliParser.xsdFile != null) {
+            File outFile = new File(cliParser.xsdFile);
+            if(outFile.exists() && !cliParser.isForceWrite) {
+                System.err.println("The outfile already exists!");
+                System.exit(2);
+            }
+            os = new FileOutputStream(outFile);
+        }
+        else {
+            os = System.out;
+        }
+
+        // Write!
+        xsdGen.write(os, charset);
     }
 }
