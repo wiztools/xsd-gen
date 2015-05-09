24c462ae6fb46936431f1400a3d3680e7710091d
Subhash Chandran <subwiz@gmail.com>
2015-05-09 10:12:56 +0530

    #4 updated the reggae for DateTime inference.

--- src/main/java/org/wiztools/xsdgen/TypeInferenceUtil.java
+++ src/main/java/org/wiztools/xsdgen/TypeInferenceUtil.java
@@ -21,7 +21,7 @@ public final class TypeInferenceUtil {
     private static final Pattern P_TIME = Pattern.compile("\\d{2}:\\d{2}:\\d{2}\\.\\d{2}");
     private static final String XSD_TIME = ":time";
 
-    private static final Pattern P_DATE_TIME = Pattern.compile("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{2}(-|\\+)\\d{2}:\\d{2}");
+    private static final Pattern P_DATE_TIME = Pattern.compile("^([\\+-]?\\d{4}(?!\\d{2}\\b))((-?)((0[1-9]|1[0-2])(\\3([12]\\d|0[1-9]|3[01]))?|W([0-4]\\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\\d|[12]\\d{2}|3([0-5]\\d|6[1-6])))([T\\s]((([01]\\d|2[0-3])((:?)[0-5]\\d)?|24\\:?00)([\\.,]\\d+(?!:))?)?(\\17[0-5]\\d([\\.,]\\d+)?)?([zZ]|([\\+-])([01]\\d|2[0-3]):?([0-5]\\d)?)?)?)?$");
     private static final String XSD_DATE_TIME = ":dateTime";
 
     private static final Pattern P_INT = Pattern.compile("-?\\d{1,9}");
